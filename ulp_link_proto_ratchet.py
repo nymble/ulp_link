43df00ca7caf5f2524fe41153d294364433f21e2
nymble <nymble@gmail.com>
2018-02-25 16:34:53 -0800

    fixed

--- ulp_link_proto_ratchet.py
+++ ulp_link_proto_ratchet.py
@@ -14,6 +14,7 @@
 from aes_siv import siv_encrypt, siv_decrypt, AES_SIV
 from hashlib import sha256 #
 import os
+from binascii import b2a_hex
 
 UDS_SIZE = 32 # size of Unique Device Secret (UDS) in octets
 UDI_SIZE = 16 # size of Unique Device Identifier (UDI) in octets
@@ -28,8 +29,7 @@ REPLAY_VALUE_SIZE = 4 #   8   two fields of this size: 'rt'  and 'rr'
 # On reset 'rt' is set to a random value and 'rr' to a string of all zeros
 ZERO_REPLAY_FIELD = REPLAY_VALUE_SIZE*'\00' # octet string of zeros
 
-REPLAY_CTR_WINDOW = 500 # number of frames allowed to be lost befor reset
-REPLAY_VALUE_LIFETIME = 100 # transmitted frames until random change in replay value
+REPLAY_VALUE_LIFETIME = 1 # transmitted frames until random change in replay value
 
 # replay value states used to implement 'ratchet'
 NEW = 1        # state after changing my_replay_value
@@ -49,19 +49,10 @@ class ULPLinkProto:
         """
         # reset my_replay protection value, and ensure it is not
         # randomly set to all zeros
-        self.change_my_replay_value()
-        self.my_replay_value = ZERO_REPLAY_FIELD
-        while self.my_replay_value != ZERO_REPLAY_FIELD:
-            self.my_replay_value = os.urandom(REPLAY_VALUE_SIZE)
-        
+        self.change_my_replay_value()  # sets replay, state and tramsmit count
+
         # the peer value is not known so set to zeros
         self.peer_replay_value = ZERO_REPLAY_FIELD
-    
-        # reset count of frames transmitted with the same 'rt'
-        self.transmitted_frame_count = 0
-
-        # reset the replay value state is one of [NEW, ECHOED]
-        self.my_replay_value_state = NEW
     
     def encrypt_frame(self, message_data):
         """ Encrypt message_data using a SIV AEAD mode
@@ -162,7 +153,7 @@ class ULPLinkProto:
         """
         # change value and ensure it is not randomly set to all zeros
         self.my_replay_value = ZERO_REPLAY_FIELD
-        while self.my_replay_value != ZERO_REPLAY_FIELD:
+        while self.my_replay_value == ZERO_REPLAY_FIELD:
             self.my_replay_value = os.urandom(REPLAY_VALUE_SIZE)
         
         # reset count of frames transmitted with the same value
@@ -171,10 +162,6 @@ class ULPLinkProto:
         # reset the replay value state
         self.my_replay_value_state = NEW
 
-# ----------------------------------------------------------
-# Ignore the rest of the code for now as it facilitates
-# vector generator and is not useful for protocol review
-
 def id_hash(uds):
     """ The hash function used the generate an identifier from
         the secret key.
@@ -182,6 +169,11 @@ def id_hash(uds):
     # sha256 for testing now, Blake2b would be much smaller and faster
     return sha256(uds).digest()[0:UDI_SIZE]   # first 16 octets / 128 bits
 
+# ----------------------------------------------------------
+# Code for design validation and not specific to link algorithm
+# remove later to different module
+#
+state_decode = {1:"NEW   ", 2:"ECHOED"}
 
 class CL(ULPLinkProto):
     """ Class to simulate a single ULPD """
@@ -190,6 +182,25 @@ class CL(ULPLinkProto):
         self._uds = uds
         self.udi = id_hash(self._uds)
         self.reset()
+
+    def send(self, message_data):
+        """ Encrypt verbose replay value output for design validation """
+        frame = self.encrypt_frame( message_data )
+        print "cl.send", b2a_hex(frame)
+        clrt = b2a_hex(self.my_replay_value)
+        clrr = b2a_hex(self.peer_replay_value)
+        state = state_decode[self.my_replay_value_state]
+        print("{} {} {} cl-->".format(state, clrt, clrr))
+        return frame
+
+    def recieve(self, frame):
+        """ Decrypt verbose replay value output for design validation """
+        message_data = self.decrypt_frame( frame )
+        clrt = b2a_hex(self.my_replay_value)
+        clrr = b2a_hex(self.peer_replay_value)
+        state = state_decode[self.my_replay_value_state]
+        print("{} {} {} cl<--".format(state, clrt, clrr))
+        return message_data
 
 
 class AD(ULPLinkProto):
@@ -202,20 +213,63 @@ class AD(ULPLinkProto):
         self.udi = ulpd.udi
         self.reset()
 
+    def send(self, message_data):
+        """ Encrypt verbose replay value output for design validation """
+        frame = self.encrypt_frame( message_data )
+        adrt = b2a_hex(self.my_replay_value)
+        adrr = b2a_hex(self.peer_replay_value)
+        state = state_decode[self.my_replay_value_state]
+        print("{} {} {}    <--ad {} {} {} ".format(6*' ',8*' ',8*' ',state, adrt, adrr))
+        return frame
+
+    def recieve(self, frame):
+        """ Decrypt verbose replay value output for design validation """
+        message_data = self.decrypt_frame( frame )
+        adrt = b2a_hex(self.my_replay_value)
+        adrr = b2a_hex(self.peer_replay_value)
+        state = state_decode[self.my_replay_value_state]
+        print("{} {} {}    -->ad {} {} {} ".format(6*' ',8*' ',8*' ',state, adrt, adrr))
+        return message_data
+
+# ---
+uds = uds = 'fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff'.decode('hex')
+class Paired_Link:
+    """ A class to simulate a paired link and display replay
+        values for design validation.
+    """
+    def __init__(self, key=uds):
+        """ Setup a pair of devices """
+        self.cl = CL(uds)
+        self.ad = AD(self.cl) # ad paired to cl
+
+    def cl_to_ad(self, message_data):
+        """ CL-to-AD Transmssion """
+        frame = self.cl.send(message_data)           # processes and displays cl state
+        ad_message_data = self.ad.recieve(frame) # processes and displays ad state
+        assert message_data == ad_message_data  # validate correct encrypt/decrypt
+
+    def ad_to_cl(self, message_data):
+        """ AD-to-CL Transmssion """
+        frame = self.ad.send(message_data)           # processes and displays cl state
+        cl_message_data = self.cl.recieve(frame) # processes and displays ad state
+        assert message_data == cl_message_data
+    
+    def cl_to_ad_dropped(self, message_data): pass
+    def ad_to_cl_dropped(self, message_data): pass
+
+
 
 def main():
-    """ Generate test vectors """
-    uds = 'fffefdfcfbfaf9f8f7f6f5f4f3f2f1f0f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff'.decode('hex')
-    ulpd = CL(uds)     # an instance of CL with fixed 'uds'
-    ad = AD(ulpd)      # instance of AD keyed for ulpd
-    
-    frame_data = 30*'\AB'  # dummy data for testing
+    """ Design Validation """
 
-    frame_0 = ulpd.encrypt_frame( frame_data )
-    print "ulpd->ad:",frame_0.encode('hex')
+    link = Paired_Link( key=uds )
+    message_data = 10*'\FA'  # dummy data for testing
 
-    data = ad.decrypt_frame( frame_0 )
-    assert data == frame_data
+    link.cl_to_ad( message_data ) # send and recieve frame
+    link.ad_to_cl( message_data )
+
+
+
 
 
 if __name__ == '__main__':
